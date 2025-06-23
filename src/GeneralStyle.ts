0c9025cae9dd760575477dc3f4badaa701ebe067
Joao H. Kohlrausch <joaohenriquekohlrausch@gmail.com>
2025-06-23 15:37:01 -0300

    Conclusão home

--- src/GeneralStyle.ts
+++ src/GeneralStyle.ts
@@ -7,19 +7,23 @@ export const MainContainer = styled.div`
     width: 100dvw;
     height: 100dvh;
     padding: 0% 0%;
-    background-image: linear-gradient(to bottom,hsl(253, 56.20%, 93.70%), #D6CCF2, #B9AEE0, #9F90C4, #7A6DAE);
+    background-image: linear-gradient(to bottom,hsl(253, 56.20%, 93.70%), #D6CCF2, #B9AEE0);
 `;
 
 export const PhoneContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
     width: 100%;
     height: 100%;
     align-self: center;
     background-color:rgb(232, 232, 233);
     box-shadow:rgb(105, 104, 104) 5px 5px 10px;
-
+    
     @media(min-width: 1000px){
-        width: 300px;
+        width: 380px;
         height: 90%;
+        max-height: 750px;
         border-radius: 15px;
     }
 `;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,25 @@
-import {MainContainer, PhoneContainer } from "../GeneralStyle";
+import { MainContainer, PhoneContainer } from "../GeneralStyle";
+import { HomeArea, SquareShapeContainer, TitleContainer, HomeTitle, HomeSquareShape, StartButton } from "./HomeStyle";
 
 const Home = () =>{
+
     return(
     <>
         <MainContainer>
             <PhoneContainer>
-                a
+                <HomeArea>
+                    <TitleContainer>
+                        <HomeTitle>NOME DO JOGO</HomeTitle>
+                    </TitleContainer>
+                    
+                    <SquareShapeContainer>
+                        <HomeSquareShape height={72} />
+                        <HomeSquareShape height={65} />
+                        <HomeSquareShape height={58} />
+                        <HomeSquareShape height={51} />
+                        <StartButton> Start! </StartButton>
+                    </SquareShapeContainer>
+                </HomeArea>
             </PhoneContainer>
         </MainContainer>
     </>
