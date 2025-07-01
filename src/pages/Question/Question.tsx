46fd46f5a1bcd054c3c2478ed453d84453d1768f
Kohlrausch <joaohenriquekohlrausch@gmail.com>
2025-06-30 22:07:39 -0300

    sistema transição de telas

--- src/pages/Question/Question.tsx
+++ src/pages/Question/Question.tsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import { MainContainer, PhoneContainer } from "../../GeneralStyle";
 import { Button, ButtonContainer, QuestionArea, TextContainer, TextSection } from "./QuestionStyle";
 
 const Question = () =>{
 
+    const [index, setIndexValue] = useState(0);
     const storyContent = [
         {
-            pageText: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit consectetur dignissimos, numquam a earum sunt ipsa, consequatur voluptatum est voluptas veritatis totam sequi. Nesciunt sunt amet, velit quod nobis ipsam.',
-            active: false
+            pageText: '1 - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit consectetur dignissimos, numquam a earum sunt ipsa, consequatur voluptatum est voluptas veritatis totam sequi. Nesciunt sunt amet, velit quod nobis ipsam.',
+        },
+        {
+            pageText: '2 - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit consectetur dignissimos, numquam a earum sunt ipsa, consequatur voluptatum est voluptas veritatis totam sequi. Nesciunt sunt amet, velit quod nobis ipsam.',
+        },
+        {
+            pageText: '3 - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit consectetur dignissimos, numquam a earum sunt ipsa, consequatur voluptatum est voluptas veritatis totam sequi. Nesciunt sunt amet, velit quod nobis ipsam.',
+        },
+        {
+            pageText: '4 - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit consectetur dignissimos, numquam a earum sunt ipsa, consequatur voluptatum est voluptas veritatis totam sequi. Nesciunt sunt amet, velit quod nobis ipsam.',
         }
     ]
 
@@ -18,15 +28,15 @@ const Question = () =>{
                 <QuestionArea>
                     <TextContainer>
                         <TextSection>
-                            d
+                            {storyContent[index].pageText}
                         </TextSection>
                     </TextContainer>
-                    
+                    {index}
                     <ButtonContainer>
-                    <Button active={false}>
+                    <Button active={(index == 0)? false : true} onClick={() => setIndexValue(index - 1)}>
                         Back
                     </Button>
-                    <Button active={true}>
+                    <Button active={(index == storyContent.length - 1)? false : true} onClick={() => setIndexValue(index + 1)}>
                         Next
                     </Button>
                     </ButtonContainer>
